ccc0bea02ee7b814530742749fb1fd33d19a0bfa
veronikavatsyak <veronikateslya@ukr.net>
2024-09-23 17:14:36 -0400

    three tasks in js

---
+++ js/task-3.js
@@ -0,0 +1,7 @@
+function getElementWidth(content, padding, border) {
+    const totalWidth = Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2;
+    return totalWidth
+}
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
